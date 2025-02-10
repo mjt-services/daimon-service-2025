7c9ced7d4cc3f69c26b6e4e16ad862d0ffb81580
Matt Taylor <matt@matthewjosephtaylor.com>
2025-02-10 09:03:27 -0600

    updated daimon.get to return Daimon

--- src/listener/daimonGetListener.ts
+++ src/listener/daimonGetListener.ts
@@ -1,18 +1,26 @@
 import type { ConnectionListener } from "@mjt-engine/message";
 import type {
+  DaimonCharaCard,
   DaimonConnectionMap,
-  DiamonCharaCard
 } from "@mjt-services/daimon-common-2025";
 import { Datas } from "../data/Datas";
 import { DAIMON_CHARA_CARD_DB_STORE } from "./DAIMON_CHARA_CARD_DB_STORE";
+import { isUndefined } from "@mjt-engine/object";
 
 export const daimonGetListener: ConnectionListener<
   DaimonConnectionMap,
   "daimon.get"
 > = async (props) => {
   const { id } = props.detail.body;
-  return Datas.get<DiamonCharaCard>({
+  const chara = await Datas.get<DaimonCharaCard>({
     dbStore: DAIMON_CHARA_CARD_DB_STORE,
     key: id,
   });
+  if (isUndefined(chara)) {
+    return undefined;
+  }
+  return {
+    id,
+    chara,
+  };
 };
