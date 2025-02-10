6c20fe8c4aa0fdb24020c76e831fac7ba319ce0a
Matt Taylor <matt@matthewjosephtaylor.com>
2025-02-10 09:17:14 -0600

    fixed bad import

--- src/listener/daimonListListener.ts
+++ src/listener/daimonListListener.ts
@@ -1,13 +1,16 @@
 import type { ConnectionListener } from "@mjt-engine/message";
 import { isUndefined, isDefined } from "@mjt-engine/object";
-import type { DaimonConnectionMap, DaimonCharaCard, Daimon } from "@mjt-services/daimon-common-2025";
+import type {
+  DaimonConnectionMap,
+  DaimonCharaCard,
+  Daimon,
+} from "@mjt-services/daimon-common-2025";
 import { Datas } from "../data/Datas";
 import { DAIMON_CHARA_CARD_DB_STORE } from "./DAIMON_CHARA_CARD_DB_STORE";
-import { undefined } from "./daimonGetListener";
-
 
 export const daimonListListener: ConnectionListener<
-  DaimonConnectionMap, "daimon.list"
+  DaimonConnectionMap,
+  "daimon.list"
 > = async (props) => {
   //TODO respect daimon.list query
   const { query } = props.detail.body;
