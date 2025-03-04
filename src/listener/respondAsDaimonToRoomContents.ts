580fc195523978ab814d904ac4019fccf3387c2e
Matt Taylor <matt@matthewjosephtaylor.com>
2025-03-04 12:09:26 -0600

    fixed issue with abort controller and stop events

--- src/listener/respondAsDaimonToRoomContents.ts
+++ src/listener/respondAsDaimonToRoomContents.ts
@@ -1,10 +1,14 @@
-import { type Daimon } from "@mjt-services/daimon-common-2025";
+import {
+  type Daimon,
+  type DaimonEventMap,
+} from "@mjt-services/daimon-common-2025";
 import { AddRoom } from "../common/AddRoom";
 import { addTextContent } from "../common/addTextContent";
 import { Daimons } from "../daimon/Daimons";
 import { getConnection } from "../getConnection";
 import type { RoomContent } from "./RoomContent";
 import { roomContentsToPrompt } from "./roomContentsToPrompt";
+import { Messages } from "@mjt-engine/message";
 
 export const respondAsDaimonToRoomContents = async ({
   roomId,
@@ -36,52 +40,87 @@ export const respondAsDaimonToRoomContents = async ({
     parentId: roomId,
   });
 
-  return new Promise((resolve, reject) => {
-    con.requestMany({
-      subject: "textgen.generate",
-      onResponse: async (response) => {
-        if (finished) {
-          return;
-        }
-        if (response.done) {
-          finished = true;
+  // TODO fix typing on the connectEventListenerToSubjectRoot function
+  const foo: DaimonEventMap = {
+    stop: "",
+  };
+  const stopGenerationAbortController = new AbortController();
+  const stopEventListenerAbortController = new AbortController();
+
+  return new Promise(async (resolve, reject) => {
+    try {
+      console.log(
+        "respondAsDaimonToRoomContents: connectEventListenerToSubjectRoot"
+      );
+      Messages.connectEventListenerToSubjectRoot<"stop", typeof foo>({
+        connection: con.connection,
+        subjectRoot: "stop",
+        signal: stopEventListenerAbortController.signal,
+        listener: async (message) => {
+          console.log("stopping", message);
+          stopGenerationAbortController.abort();
+          reject(new Error("stopped"));
+        },
+      });
+      console.log("respondAsDaimonToRoomContents: requestMany");
+      await con.requestMany({
+        subject: "textgen.generate",
+        signal: stopGenerationAbortController.signal,
+
+        onResponse: async (response) => {
+          if (finished) {
+            return;
+          }
+          const text =
+            response.text?.replace(new RegExp(`^ *${assistantName}: *`), "") ??
+            "";
+          console.log(text);
+          if (response.done) {
+            finished = true;
+            await addTextContent({
+              creatorId: daimon.id,
+              text,
+              contentId,
+              createdAt,
+              finalized: true,
+            });
+            resolve(contentId);
+            return;
+          }
           await addTextContent({
             creatorId: daimon.id,
-            text: response.text ?? "",
+            text,
             contentId,
             createdAt,
-            finalized: true,
+            finalized: false,
           });
-          resolve(contentId);
-          return;
-        }
-        await addTextContent({
-          creatorId: daimon.id,
-          text: response.text ?? "",
-          contentId,
-          createdAt,
-          finalized: false,
-        });
-      },
-      request: {
-        body: {
-          // model: "google/gemini-2.0-flash-001",
-          // model: "mistralai/mistral-nemo",
-          // model: "google/gemini-flash-1.5",
-          model: "gryphe/mythomax-l2-13b",
-          stream: true,
-          messages: [
-            {
-              role: "system",
-              content: fullSystemPrompt,
-            },
-            {
-              role: "user",
-              content: `Give ${assistantName} next response. Begin with '${assistantName}: '`,
-            },
-          ],
         },
-      },
-    });
+        request: {
+          body: {
+            // model: "google/gemini-2.0-flash-001",
+            // model: "mistralai/mistral-nemo",
+            // model: "google/gemini-flash-1.5",
+            model: "gryphe/mythomax-l2-13b",
+            stream: true,
+            messages: [
+              {
+                role: "system",
+                content: fullSystemPrompt,
+              },
+              {
+                role: "user",
+                content: `Give ${assistantName}'s next response. Begin with '${assistantName}:'`,
+              },
+            ],
+          },
+        },
+      });
+    } catch (error) {
+      console.log("error in respondAsDaimonToRoomContents", error);
+      reject(error);
+    } finally {
+      console.log("wrapping up respondAsDaimonToRoomContents");
+      stopEventListenerAbortController.abort();
+    }
   });
 };
