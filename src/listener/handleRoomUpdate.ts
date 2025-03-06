9a314b4b75cc8db17f70c1e22c761ae37345021d
Matt Taylor <matt@matthewjosephtaylor.com>
2025-03-06 08:04:07 -0600

    better descrimination of user room-messages.

--- src/listener/handleRoomUpdate.ts
+++ src/listener/handleRoomUpdate.ts
@@ -12,19 +12,26 @@ export const handleRoomUpdate = async (roomId: string) => {
   if (isUndefined(last)) {
     return;
   }
-  if (isDefined(last.content?.creatorId)) {
-    return;
-  }
   const daimons = (await findDaimonsByRoom(roomId)).toSorted((a, b) => {
     if (a.chara.data.name && b.chara.data.name) {
       return a.chara.data.name.localeCompare(b.chara.data.name);
     }
     return 0;
   });
+  console.log("Daimons: ", daimons);
   const userDaimon = daimons.find((d) => d.chara.data.extensions?.isUser);
+  console.log("User daimon: ", userDaimon);
 
+  if (last.content?.creatorId !== userDaimon?.id) {
+    console.log("skipping, last content creator is not user daimon", {
+      lastCreator: last.content?.creatorId,
+      userDaimonId: userDaimon?.id,
+    });
+    return;
+  }
   for (const daimon of daimons) {
     if (daimon.chara.data.extensions?.isUser) {
+      console.log("Skipping user daimon: ", daimon);
       continue;
     }
     const roomChildren = await findRoomChildren(roomId);
